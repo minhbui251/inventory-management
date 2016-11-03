fd84c0d136fe863255754b42d998c183a72c0187
Minh Bui <minhbui@uark.edu>
2016-11-03 13:29:01 -0500

    Create Product Screen

---
+++ inventory management/Create Product From.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventory_management
+{
+    public partial class Create_Product_From : Form
+    {
+        public Create_Product_From()
+        {
+            InitializeComponent();
+        }
+    }
+}
